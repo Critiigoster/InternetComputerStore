6044070400a02c1dd80f9e578035e5c585c27324
Ihor Holovanov <begroom111@gmail.com>
2021-02-08 10:47:19 +0200

    Added new bootstrap files "boorstrap-flatly.css" Test using of models/controller/view

---
+++ InternetComputerStore/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InternetComputerStore.Models;
+
+namespace InternetComputerStore.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Random()
+        {
+            var trying = new Test() { Name = "DeveloperTryer" }; 
+            return View(trying);
+        }
+    }
+}
